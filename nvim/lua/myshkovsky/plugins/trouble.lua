1ea8de3c3f7cdee351e8dcc947f5b9238148d4f6
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-02-15 01:15:18 +0100

    feat: add trouble.nvim

---
+++ nvim/lua/myshkovsky/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup({})
+
+            vim.keymap.set("n", "<leader>xx", function()
+                require("trouble").toggle()
+            end)
+
+            vim.keymap.set("n", "[x", function()
+                require("trouble").next({ skip_groups = true, jump = true });
+            end)
+
+            vim.keymap.set("n", "]x", function()
+                require("trouble").previous({ skip_groups = true, jump = true });
+            end)
+        end
+    },
+}
