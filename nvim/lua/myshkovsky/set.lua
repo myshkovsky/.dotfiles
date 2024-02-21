50110f39ef329cc19b80f4d1a2a02b7a7aedf052
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-02-21 17:27:57 +0100

    refactor: path changes for ubuntu

--- nvim/lua/myshkovsky/set.lua
+++ nvim/lua/myshkovsky/set.lua
@@ -18,14 +18,14 @@ vim.opt.termguicolors = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOMEPATH") .. "/.vim/undodir"
+vim.opt.undodir = "~/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
 -- I LOOOOOOVE FILESYSTEMS 😻😻😻
-vim.opt.isfname = "@,48-57,/,\\,.,-,_,+,,,#,$,%,:,@-@,!,~,="
+-- vim.opt.isfname = "@,48-57,/,\\,.,-,_,+,,,#,$,%,:,@-@,!,~,="
 
 vim.opt.updatetime = 50
 
