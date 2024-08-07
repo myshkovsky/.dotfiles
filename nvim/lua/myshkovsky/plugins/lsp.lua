f4dceda696f5323b0d03966e9b1a68af93d98a43
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-08-07 10:43:50 +0200

    feat(lsp-zero): bump to v4.x

--- nvim/lua/myshkovsky/plugins/lsp.lua
+++ nvim/lua/myshkovsky/plugins/lsp.lua
@@ -1,90 +1,110 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    dependencies = {
-        -- LSP Support
-        { 'neovim/nvim-lspconfig' },
-        { 'williamboman/mason.nvim' },
-        { 'williamboman/mason-lspconfig.nvim' },
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'saadparwaiz1/cmp_luasnip' },
-        { 'hrsh7th/cmp-nvim-lua' },
-        -- Snippets
-        { 'L3MON4D3/LuaSnip' },
-        { 'rafamadriz/friendly-snippets' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
+        lazy = true,
+        config = false,
     },
-    config = function()
-        local lsp_zero = require('lsp-zero')
+    {
+        'williamboman/mason.nvim',
+        lazy = false,
+        config = true,
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
 
-        lsp_zero.on_attach(function(_, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
+        dependencies = {
+            { 'L3MON4D3/LuaSnip' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'rafamadriz/friendly-snippets' },
+        },
+        config = function()
+            local cmp = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            require('luasnip.loaders.from_vscode').lazy_load()
 
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            ensure_installed = {
-                'tsserver',
-                'eslint',
-                'tailwindcss',
-                'lua_ls',
-                'sqlls',
-                'gopls',
-                'svelte',
-                'ruff',
-                'ruff_lsp',
-                'pyright',
-            },
-            handlers = {
-                lsp_zero.default_setup,
-                lua_ls = function()
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
-                end,
-            }
-        })
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp', group_index = 0 },
+                    { name = 'luasnip',  group_index = 1 },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+            })
+        end
+    },
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
 
-        local cmp = require('cmp')
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            local lsp_attach = function(_, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            end
 
-        require('luasnip.loaders.from_vscode').lazy_load()
+            lsp_zero.extend_lspconfig({
+                sign_text = true,
+                lsp_attach = lsp_attach,
+                capabilities = require('cmp_nvim_lsp').default_capabilities()
+            })
 
-        cmp.setup({
-            sources = {
-                { name = 'path' },
-                { name = 'nvim_lsp', keyword_length = 3, group_index = 0, max_item_count = 50 },
-                { name = 'nvim_lua' },
-                { name = 'luasnip',  keyword_length = 2 },
-                { name = 'buffer',   keyword_length = 3 },
-            },
-            formatting = lsp_zero.cmp_format(),
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-Space>'] = cmp.mapping.complete(),
-            }),
-        })
-
-        cmp.setup.filetype('norg', {
-            sources = cmp.config.sources({})
-        })
-
-        vim.diagnostic.config({
-            virtual_text = true
-        })
-    end
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    'lua_ls',
+                    'sqlls',
+                    'gopls',
+                    'pyright',
+                    'ruff_lsp',
+                    -- Web development
+                    'html',
+                    'cssls',
+                    'eslint',
+                    'tsserver',
+                    'tailwindcss'
+                },
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
+                }
+            })
+        end
+    }
 }
